9cff4a4a2a1d6009377f323f230534beadc90bff
Удалов Сергей <udalov.sergey1995@yandex.ru>
2021-11-25 01:40:28 +0300

    Добавил запрос на получение всех объктов из таблицы

---
+++ WebApplication1/Library/DB/GetAllObjectsFromTable.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication1.Library.DB
+{
+    public class GetAllObjectsFromTable<T> : IDBRequest<IEnumerable<T>>
+    {
+        public async Task<IEnumerable<T>> Send(string tableName)
+        {
+            IEnumerable<T> result = null;
+            using (var db = new MySqlConnection(SQLConfig.Connection))
+            {
+                var resp = await db.QueryMultipleAsync("SELECT * FROM " + tableName);
+                result = resp.Read<T>();
+            }
+            return result;
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,16 +1,17 @@
+using Newtonsoft.Json;
 using WebApplication1.Library;
 using WebApplication1.Library.DB;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var t = new GetAllObjectsFromTable<UserModel>();
 
-var t = new GetObjectById<UserModel>("users");
+var result = await t.Send("users");
 
-var result = await t.Send("2");
-var p = result.Passport;
-var d = result.Department;
+var JSON = JsonConvert.SerializeObject(result);
+
 //t.Send("INSERT INTO `users`(`id`, `Name`, `Surname`, `Phone`, `CompanyId`, `PassportId`, `DepartmentId`) VALUES (1,\"\",\"\",\"\",1,1,1)");
 
+app.MapGet("/", () => JSON);
 app.Run();
